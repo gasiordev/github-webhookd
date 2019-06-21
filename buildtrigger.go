760c05ee33ec0a07cb571290baf3c032ce9da950
Nicholas Gasior <nicholas@laatu.org>
2019-06-21 11:19:37 +0200

    Parse new config file that defines Jenkins URL's #1

--- buildtrigger.go
+++ buildtrigger.go
@@ -25,14 +25,14 @@ func (trig *BuildTrigger) GetConfig() *Config {
 	return &(trig.config)
 }
 
-func (trig *BuildTrigger) Init(cfgFile string) {
-	cfgJSON, err := ioutil.ReadFile(cfgFile)
+func (trig *BuildTrigger) Init(p string) {
+	c, err := ioutil.ReadFile(p)
 	if err != nil {
 		log.Fatal("Error reading config file")
 	}
 
 	var cfg Config
-	cfg.SetFromJSON(cfgJSON)
+	cfg.SetFromJSON(c)
 	trig.config = cfg
 }
 
@@ -54,16 +54,156 @@ func (trig *BuildTrigger) startTriggerAPI() {
 	log.Fatal(http.ListenAndServe(":"+trig.config.Port, router))
 }
 
+func (trig *BuildTrigger) getJenkinsEndpointRetryCount(e *JenkinsEndpoint) (int, error) {
+	rc := int(1)
+	if e.Retry.Count != "" {
+		i, err := strconv.Atoi(e.Retry.Count)
+		if err != nil {
+			return 0, errors.New("Value of Retry.Count cannot be converted to int")
+		}
+		rc = i
+	}
+	return rc, nil
+}
+
+func (trig *BuildTrigger) getJenkinsEndpointRetryDelay(e *JenkinsEndpoint) (int, error) {
+	rd := int(0)
+	if e.Retry.Delay != "" {
+		i, err := strconv.Atoi(e.Retry.Count)
+		if err != nil {
+			return 0, errors.New("Value of Retry.Delay cannot be converted to int")
+		}
+		rd = i
+	}
+	return rd, nil
+}
+
+func (trig *BuildTrigger) processJenkinsEndpoint(e string, repo string, branch string) error {
+	endp := trig.config.Jenkins.EndpointsMap[e]
+	if endp == nil {
+		return nil
+	}
+
+	rd, err := trig.getJenkinsEndpointRetryDelay(endp)
+	if err != nil {
+		return nil
+	}
+	rc, err := trig.getJenkinsEndpointRetryCount(endp)
+	if err != nil {
+		return nil
+	}
+
+	return trig.processJenkinsEndpointRetries(endp, repo, branch, rd, rc)
+}
+
+func (trig *BuildTrigger) printIteration(i int, rc int) {
+	log.Print("Retry: (" + strconv.Itoa(i+1) + "/" + strconv.Itoa(rc) + ")")
+}
+
+func (trig *BuildTrigger) getCrumbAndSleep(u string, t string, rd int) (string, error) {
+	crumb, err := trig.getCrumb(u, t)
+	if err != nil {
+		log.Print("Error getting crumb")
+		time.Sleep(time.Second * time.Duration(rd))
+		return "", errors.New("Error getting crumb")
+	}
+	return crumb, nil
+}
+
+func (trig *BuildTrigger) replacePathWithRepoAndBranch(p string, r string, b string) string {
+	s := strings.ReplaceAll(p, "{{.repository}}", r)
+	s = strings.ReplaceAll(s, "{{.branch}}", b)
+	return s
+}
+
+func (trig *BuildTrigger) compareHTTPStatusAndSleep(rsCode int, es string, rd int) (bool, error) {
+	esCode, err := strconv.Atoi(es)
+	if err != nil {
+		return false, errors.New("Error converting Success.HTTPStatus to int")
+	}
+	if rsCode != esCode {
+		rs := strconv.Itoa(rsCode)
+		log.Print("HTTP Status " + rs + " different than expected " + es)
+		time.Sleep(time.Second * time.Duration(rd))
+		return false, nil
+	}
+	return true, nil
+}
+
+func (trig *BuildTrigger) processJenkinsEndpointRetries(endpointDef *JenkinsEndpoint, repo string, branch string, retryDelay int, retryCount int) error {
+	iterations := int(0)
+	if retryCount > 0 {
+		for iterations < retryCount {
+			trig.printIteration(iterations, retryCount)
+
+			crumb, err := trig.getCrumbAndSleep(trig.config.Jenkins.User, trig.config.Jenkins.Token, retryDelay)
+			if err != nil {
+				iterations++
+				continue
+			}
+
+			endpointPath := trig.replacePathWithRepoAndBranch(endpointDef.Path, repo, branch)
+
+			req, err := http.NewRequest("POST", trig.config.Jenkins.BaseURL+"/"+endpointPath, strings.NewReader(""))
+			if err != nil {
+				log.Print("Error creating request to " + endpointPath)
+				time.Sleep(time.Second * time.Duration(retryDelay))
+				iterations++
+				continue
+			}
+
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+			req.SetBasicAuth(trig.config.Jenkins.User, trig.config.Jenkins.Token)
+			req.Header.Add("Jenkins-Crumb", crumb)
+			c := &http.Client{}
+			resp, err := c.Do(req)
+			if err != nil {
+				log.Print("Error from request to " + endpointPath)
+				time.Sleep(time.Second * time.Duration(retryDelay))
+				iterations++
+				continue
+			}
+			resp.Body.Close()
+
+			log.Print("Posted to endpoint " + endpointPath)
+
+			if endpointDef.Success.HTTPStatus != "" {
+				cmp, err := trig.compareHTTPStatusAndSleep(resp.StatusCode, endpointDef.Success.HTTPStatus, retryDelay)
+				if err != nil {
+					return err
+				}
+				if !cmp {
+					iterations++
+					continue
+				}
+			}
+
+			return nil
+		}
+	}
+	return errors.New("Unable to post to endpoint " + endpointDef.Path)
+}
+
 func (trig *BuildTrigger) ProcessGitHubPayload(b *([]byte)) error {
 	j := make(map[string]interface{})
 	err := json.Unmarshal(*b, &j)
 	if err == nil {
-		log.Print("Got payload from GitHub")
 		if j["pusher"] != nil && j["ref"] != nil && j["repository"] != nil && j["repository"].(map[string]interface{})["name"] != nil {
 			ref := strings.Split(j["ref"].(string), "/")
 			if ref[1] != "tag" {
-				log.Print("Got payload to process")
-				return trig.triggerMultibranchJob(j["repository"].(map[string]interface{})["name"].(string), ref[2])
+				log.Print("Got payload from GitHub to process")
+
+				if trig.config.EndpointsToTrigger.Jenkins != nil {
+					for _, e := range trig.config.EndpointsToTrigger.Jenkins {
+						err := trig.processJenkinsEndpoint(e, j["repository"].(map[string]interface{})["name"].(string), ref[2])
+						if err != nil {
+							log.Print("Error processing endpoint " + e + ". Breaking.")
+							break
+						}
+					}
+				}
+
+				return nil
 			}
 		}
 	} else {
@@ -72,52 +212,13 @@ func (trig *BuildTrigger) ProcessGitHubPayload(b *([]byte)) error {
 	return nil
 }
 
-func (trig *BuildTrigger) triggerMultibranchJob(repo string, branch string) error {
-	err := trig.triggerMultibranchJobScan(repo)
-	if err != nil {
-		return errors.New("Error sending POST to multibranch job scan of " + repo)
-	}
-
-	crumb, _ := trig.getCrumb(trig.config.JenkinsUser, trig.config.JenkinsToken)
-
-	iterations := int(0)
-	for iterations < 20 {
-		req, err := http.NewRequest("POST", trig.config.JenkinsURL+"/job/"+repo+"_multibranch/job/"+branch+"/build", strings.NewReader(""))
-		if err != nil {
-			log.Print("Error creating request for building multibranch job")
-			time.Sleep(time.Second * time.Duration(5))
-			iterations++
-			continue
-		}
-
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.SetBasicAuth(trig.config.JenkinsUser, trig.config.JenkinsToken)
-		req.Header.Add("Jenkins-Crumb", crumb)
-		c := &http.Client{}
-		resp, err := c.Do(req)
-		defer resp.Body.Close()
-
-		if err == nil && resp.StatusCode == 201 {
-			log.Print("Found and triggered branch " + branch + " in " + repo)
-			return nil
-		}
-
-		log.Print("Couldn't find branch " + branch + " in " + repo + " (" + strconv.Itoa(iterations+1) + "/20)")
-		time.Sleep(time.Second * time.Duration(5))
-
-		iterations++
-	}
-
-	return errors.New("Timeout waiting for branch existance")
-}
-
 func (trig *BuildTrigger) getCrumb(user string, token string) (string, error) {
-	req, err := http.NewRequest("GET", trig.config.JenkinsURL+"/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)", strings.NewReader(""))
+	req, err := http.NewRequest("GET", trig.config.Jenkins.BaseURL+"/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)", strings.NewReader(""))
 	if err != nil {
 		return "", err
 	}
 
-	req.SetBasicAuth(trig.config.JenkinsUser, trig.config.JenkinsToken)
+	req.SetBasicAuth(trig.config.Jenkins.User, trig.config.Jenkins.Token)
 	c := &http.Client{}
 	resp, err := c.Do(req)
 	if err != nil {
@@ -127,26 +228,4 @@ func (trig *BuildTrigger) getCrumb(user string, token string) (string, error) {
 	defer resp.Body.Close()
 	b, _ := ioutil.ReadAll(resp.Body)
 	return strings.Split(string(b), ":")[1], nil
-}
-
-func (trig *BuildTrigger) triggerMultibranchJobScan(repo string) error {
-	crumb, _ := trig.getCrumb(trig.config.JenkinsUser, trig.config.JenkinsToken)
-	req, err := http.NewRequest("POST", trig.config.JenkinsURL+"/job/"+repo+"_multibranch/build", strings.NewReader(""))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.SetBasicAuth(trig.config.JenkinsUser, trig.config.JenkinsToken)
-	req.Header.Add("Jenkins-Crumb", crumb)
-	c := &http.Client{}
-	resp, err := c.Do(req)
-	if err != nil {
-		return err
-	}
-	resp.Body.Close()
-
-	log.Print("Triggered multibranch job scan for " + repo)
-
-	return nil
 }
--- config.go
+++ config.go
@@ -6,16 +6,47 @@ import (
 )
 
 type Config struct {
-	Version      string `json:"version"`
-	Port         string `json:"port"`
-	JenkinsURL   string `json:"jenkins_url"`
-	JenkinsUser  string `json:"jenkins_user"`
-	JenkinsToken string `json:"jenkins_token"`
+	Version            string             `json:"version"`
+	Port               string             `json:"port"`
+	Jenkins            Jenkins            `json:"jenkins"`
+	EndpointsToTrigger EndpointsToTrigger `json:"endpoints_to_trigger"`
+}
+
+type Jenkins struct {
+	User         string            `json:"user"`
+	Token        string            `json:"token"`
+	BaseURL      string            `json:"base_url"`
+	Endpoints    []JenkinsEndpoint `json:"endpoints"`
+	EndpointsMap map[string]*JenkinsEndpoint
+}
+
+type JenkinsEndpoint struct {
+	Id      string                 `json:"id"`
+	Path    string                 `json:"path"`
+	Retry   JenkinsEndpointRetry   `json:"retry"`
+	Success JenkinsEndpointSuccess `json:"success"`
+}
+
+type JenkinsEndpointRetry struct {
+	Delay string `json:"delay"`
+	Count string `json:"count"`
+}
+
+type JenkinsEndpointSuccess struct {
+	HTTPStatus string `json:"http_status"`
+}
+
+type EndpointsToTrigger struct {
+	Jenkins []string `json:"jenkins"`
 }
 
 func (c *Config) SetFromJSON(b []byte) {
 	err := json.Unmarshal(b, c)
 	if err != nil {
 		log.Fatal("Error setting config from JSON:", err.Error())
+	}
+	c.Jenkins.EndpointsMap = make(map[string]*JenkinsEndpoint)
+	for i, e := range c.Jenkins.Endpoints {
+		c.Jenkins.EndpointsMap[e.Id] = &(c.Jenkins.Endpoints[i])
 	}
 }
